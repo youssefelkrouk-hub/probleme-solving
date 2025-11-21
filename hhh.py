39a1e5031bfa6638ccd9ac4085956d0d70e832ce
Youssef EL KROUK <sbirakrouk@gmail.com>
2025-11-21 21:43:48 +0100

    Fix typo in class name from 'Personne' to 'Persone'

--- hhh.py
+++ hhh.py
@@ -5,7 +5,7 @@ class Persone:
         self.name=name
 
 perso_1=Persone("Youssef",21)
-pers_2=Personne("Ahmed",34)
+pers_2=Persone("Ahmed",34)
 print(perso_1.name)
 print(perso_1.age)
 print(perso_2.name)
