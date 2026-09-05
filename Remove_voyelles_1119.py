bac51b26af5519c16cd10ead15f8e5313ea067a0
youssefelkrouk-hub <sbirakrouk@gmail.com>
2026-09-05 03:45:26 +0100

    String are Immutable

---
+++ Remove_voyelles_1119.py
@@ -0,0 +1,11 @@
+j="YOUSSEF DATA"
+b="".join(char for char in j )
+print(b)
+
+
+## Anther way to create a string based on anothe roe  
+# S="YOUSSEF DATA"
+empty_string=""
+for s in j:
+    empty_string+=s
+print(empty_string)
