058da606a621d8947f9f22d683f694c75fc1be34
youssefelkrouk-hub <sbirakrouk@gmail.com>
2026-09-05 18:37:49 +0100

    Tow Sum in a sorted array using Binary search  O(nlogn)

--- Tow_Sum.py
+++ Tow_Sum.py
@@ -7,11 +7,9 @@ def brute_force(nums,target):
                 return [i,j]
     return []
 
-
 nums = [3,2,4]
 target = 6
 print("Tow Sum Problem using a brute force way:",brute_force(nums,target),"\n")
-
 
 
 
@@ -22,15 +20,13 @@ def tow_sum(nums,target):
         if compliment in map: #real check if it's exist in the map 
             return [map[compliment],index] 
         map[number]=index # if no, add or insert the (key,value)=(number,index)
-
-    
+  
 nums = [2, 7, 11, 15]
 target = 9
 print("The optimized way using Hashmap , where time complexity O(n),adn space complexity O(n) : ",tow_sum(nums,target),"\n")
 
 
-# The tow_sum problem || , now the arrays is sorted : 
-from typing import List
+# The tow_sum problem || , now the arrays is sorted : usiin tow pointers technique 
 
 def tow_sum_2(numbers,target):
     n,m=0,len(numbers)-1
@@ -42,12 +38,52 @@ def tow_sum_2(numbers,target):
             n+=1
         else:
             m-=1
-    return []
+    return [-1,-1]
 
 
 nums = [2, 7, 11, 15]
 target = 9
 print("Using tow Pointers to solve the tow sum for an array that is sorted ",print(tow_sum_2(nums,target)),"\n")
+
+
+# Tow SUM || : Sorted Array 
+# ------> Intuition  <----------: 
+# Aproach 2: using a  binary search : 
+# Fix one element and search for its complement. 
+# For each numbers[i], 
+# the value we need is complement = target - numbers[i]. 
+# Since the array is sorted, 
+# a binary search on the portion after index i finds that complement in O(log n) 
+# instead of the O(n) linear scan.  with a simple for loop  ! 
+
+
+def tow_sum_binary_search(nums,target):
+    n=len(nums)
+    for i in range(n-1):
+        complement=target-nums[i]
+        left,right=i+1,n-1
+        while left<=right:
+            mid = (left + right) // 2
+            if nums[mid]==complement:
+                return [i+1,mid+1]
+            elif nums[mid]<complement:
+                left=mid+1
+            else:
+                right=mid-1 
+                
+nums=[-3,-1,0,2,4,6]
+target=3
+print("Using Binary search to catch the complement that w'ill add up with val = Target :  ",tow_sum_binary_search(nums,target),"\n")
+
+
+
+
+
+
+
+
+
+
 
 def tow_sum(nums,target):
     map={}
